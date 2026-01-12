9ebf52d5bc0acf5564c7671b835d0f7232c0768e
Yasserkhayyal <yasser.khayal@gmail.com>
2026-01-12 21:35:09 +0200

    fix typography setup

--- core/design-system/src/main/java/com/khayal/designsystem/theme/Type.kt
+++ core/design-system/src/main/java/com/khayal/designsystem/theme/Type.kt
@@ -2,6 +2,8 @@ package com.khayal.designsystem.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.googlefonts.Font
 import androidx.compose.ui.text.googlefonts.GoogleFont
 import com.khayal.designsystem.R
@@ -12,37 +14,46 @@ val provider = GoogleFont.Provider(
     certificates = R.array.com_google_android_gms_fonts_certs
 )
 
-val bodyFontFamily = FontFamily(
-    Font(
-        googleFont = GoogleFont("Inter"),
-        fontProvider = provider,
-    )
-)
-
-val displayFontFamily = FontFamily(
-    Font(
-        googleFont = GoogleFont("Inter"),
-        fontProvider = provider,
-    )
+val fontStyles = listOf(FontStyle.Normal, FontStyle.Italic)
+val appFontFamily = FontFamily(
+    listOf(
+        FontWeight.Thin,
+        FontWeight.ExtraLight,
+        FontWeight.Light,
+        FontWeight.Normal,
+        FontWeight.Medium,
+        FontWeight.SemiBold,
+        FontWeight.Bold,
+        FontWeight.ExtraBold
+    ).flatMap { weight ->
+        fontStyles.map { style ->
+            Font(
+                googleFont = GoogleFont("Inter"),
+                fontProvider = provider,
+                weight = weight,
+                style = style
+            )
+        }
+    }
 )
 
 // Default Material 3 typography values
 val baseline = Typography()
 
 val AppTypography = Typography(
-    displayLarge = baseline.displayLarge.copy(fontFamily = displayFontFamily),
-    displayMedium = baseline.displayMedium.copy(fontFamily = displayFontFamily),
-    displaySmall = baseline.displaySmall.copy(fontFamily = displayFontFamily),
-    headlineLarge = baseline.headlineLarge.copy(fontFamily = displayFontFamily),
-    headlineMedium = baseline.headlineMedium.copy(fontFamily = displayFontFamily),
-    headlineSmall = baseline.headlineSmall.copy(fontFamily = displayFontFamily),
-    titleLarge = baseline.titleLarge.copy(fontFamily = displayFontFamily),
-    titleMedium = baseline.titleMedium.copy(fontFamily = displayFontFamily),
-    titleSmall = baseline.titleSmall.copy(fontFamily = displayFontFamily),
-    bodyLarge = baseline.bodyLarge.copy(fontFamily = bodyFontFamily),
-    bodyMedium = baseline.bodyMedium.copy(fontFamily = bodyFontFamily),
-    bodySmall = baseline.bodySmall.copy(fontFamily = bodyFontFamily),
-    labelLarge = baseline.labelLarge.copy(fontFamily = bodyFontFamily),
-    labelMedium = baseline.labelMedium.copy(fontFamily = bodyFontFamily),
-    labelSmall = baseline.labelSmall.copy(fontFamily = bodyFontFamily)
+    displayLarge = baseline.displayLarge.copy(fontFamily = appFontFamily),
+    displayMedium = baseline.displayMedium.copy(fontFamily = appFontFamily),
+    displaySmall = baseline.displaySmall.copy(fontFamily = appFontFamily),
+    headlineLarge = baseline.headlineLarge.copy(fontFamily = appFontFamily),
+    headlineMedium = baseline.headlineMedium.copy(fontFamily = appFontFamily),
+    headlineSmall = baseline.headlineSmall.copy(fontFamily = appFontFamily),
+    titleLarge = baseline.titleLarge.copy(fontFamily = appFontFamily),
+    titleMedium = baseline.titleMedium.copy(fontFamily = appFontFamily),
+    titleSmall = baseline.titleSmall.copy(fontFamily = appFontFamily),
+    bodyLarge = baseline.bodyLarge.copy(fontFamily = appFontFamily),
+    bodyMedium = baseline.bodyMedium.copy(fontFamily = appFontFamily),
+    bodySmall = baseline.bodySmall.copy(fontFamily = appFontFamily),
+    labelLarge = baseline.labelLarge.copy(fontFamily = appFontFamily),
+    labelMedium = baseline.labelMedium.copy(fontFamily = appFontFamily),
+    labelSmall = baseline.labelSmall.copy(fontFamily = appFontFamily)
 )
